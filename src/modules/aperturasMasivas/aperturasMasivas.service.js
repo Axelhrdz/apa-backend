9ae588f195bebb61fe1788274741e8ca63d55e6d
Axel mac <axelhernandezperez23@gmail.com>
2026-02-04 23:33:13 -0600

    feat(aperturas-masivas): process xlsx file and generate txt file

--- src/modules/aperturasMasivas/aperturasMasivas.service.js
+++ src/modules/aperturasMasivas/aperturasMasivas.service.js
@@ -1,14 +1,56 @@
-const aperturasMasivasService = async (req) => {
+import * as fs from 'fs';
+import { text } from 'stream/consumers';
+import * as XLSX from 'xlsx';
 
+const txtFile = (jsonData) => {
+    let textChainFormat = '';
+    const accountsToProcess = [];
+
+    jsonData.forEach(account => {
+        textChainFormat = `${account['Recaudadora']},${account['Tipo']},${account['Cuenta']},N,,,,4,1,${account['Fecha de otorgamiento']},,H,N,0,${account['Recamaras']},${account['Banios']}`;
+        accountsToProcess.push(textChainFormat);
+    });
+
+    const txtContent = accountsToProcess.join('\n');
+    console.log(txtContent);
+
+    //write string into file
+    fs.writeFileSync('output.txt', txtContent, 'utf-8');
+    console.log('File written successfully');
+
+    return accountsToProcess;
+}
+
+const aperturasMasivasService = async (req) => {
     const file = req.files.file;
     const formData = req.body;
 
-    // console.log('Data received from service');
-    // console.log(file);
-    // console.log(formData);
+    console.log('Data received from service');
+    console.log(file);
+    console.log(formData);
+
+    try {
+        //read file as buffer
+        // const fileBuffer = file.data.toString('base64');
+        const workbook = XLSX.read(file.data, {type: 'buffer'});
+
+        //get first worksheet name
+        const firstSheetName = workbook.SheetNames[0];
+        const worksheet = workbook.Sheets[firstSheetName];
+
+        //convert to an array of json obejcts
+        const jsonData = XLSX.utils.sheet_to_json(worksheet);
 
 
-    
+        return txtFile(jsonData);
+        
+    } catch (error) {
+        console.error('Error in aperturas masivas service', error);
+        return {
+            message: 'Error in aperturas masivas service',
+            error: error.message
+        }
+    }
 
 
 
